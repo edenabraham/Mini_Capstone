80b3ba44a0630640e7b321d8fe643f1e27ca71c0
Eden Abraham <eabraham@atl.yearup.org>
2022-10-02 22:37:11 -0400

    just took some notes out

--- Calculators/Value_of_annuity.js
+++ Calculators/Value_of_annuity.js
@@ -1,6 +1,4 @@
-// To fund an annuity that pays $3,000 monthly for 20 years
-// and earns an expected 2.5% interest, you would need to invest
-//$566,141.46 today.
+// 
 
  var interestRate=2.5; 
  
